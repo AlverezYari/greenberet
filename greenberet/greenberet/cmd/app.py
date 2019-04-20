2828f384823053d6bd9daa679b192fb69b533372
cphillips <cphillips@cp-nix01.phillipshome>
2019-04-20 14:42:03 -0500

    Misc structure

--- greenberet/greenberet/cmd/app.py
+++ greenberet/greenberet/cmd/app.py
@@ -1,14 +1,16 @@
 import click
-from ruamel.yaml import YAML
 
 @click.group()
+@click.option('--version', help="Prints your green beret version number")
 @click.pass_context
-def main (ctx):
+def main (ctx,version):
     '''
-    Main GB Cli
+    Green Beret the Python Brigade Client
     '''
-    click.echo('Main CLI')
+    # null:click.echo('Main CLI')
     pass
+
+# Cluster Commands
 
 @main.command()
 @click.option('-n','--name',  help='Your cluster name', confirmation_prompt=True)
@@ -25,7 +27,31 @@ def cluster(ctx,name,server,info):
     else:
         click.echo('No info')
 
+# Project Commands
 
+@main.command()
+@click.option('-l','--list',  help='List project deployed in your target cluster', is_flag=True)
+@click.option('-g','--get', help='Get a specific project by id', type=str)
+@click.option('-c', '--create', help="Create a new project", )
+@click.option('-d','--delete', help='Deletes project from cluster', type=str)
+@click.pass_context
+def project(ctx,list,get,create,delete):
+    '''
+    Project Functions 
+    '''
+    if list:
+        #Call list function
+        click.echo('List Projects')
+    elif get != None:
+        #call get function
+        click.echo('Get Projects')
+    elif create != None:
+        #call Create function
+        click.echo('Create a new project')
+    elif delete != None:
+        #call Delete function
+        click.echo('Delete a project')
+    pass
 
 
 def start():
